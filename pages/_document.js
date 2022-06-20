3918ef0b82256647d695133b977d49a6f09d0345
Erica Huang <57018573+hutingerica@users.noreply.github.com>
2022-06-20 15:14:10 +0100

    set up GA

--- pages/_document.js
+++ pages/_document.js
@@ -46,6 +46,22 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <meta name="yandex-verification" content="ece5ab476686d015" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
